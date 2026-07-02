a205f35762aac404b35e550470f99d7232bb4d9d
tuyen <hoangtuyen1106@gmail.com>
2026-07-03 00:38:30 +0700

    12. Nx Prisma Targets

--- apps/auth/src/app/prisma/prisma.service.ts
+++ apps/auth/src/app/prisma/prisma.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const pool = new Pool({ connectionString: process.env.AUTH_DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+}
