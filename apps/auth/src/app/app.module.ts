7e50a19b3f411133353032a1b0df78abc058bc95
tuyen <hoangtuyen1106@gmail.com>
2026-07-04 16:47:41 +0700

    14. GraphQL & Nx Libs - Part 2

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule,
+  imports: [
+    PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-  ]
+    UsersModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
