3b40fe2531ef38cdcb7c6101ab7a0790a504716e
tuyen <hoangtuyen1106@gmail.com>
2026-07-06 23:18:28 +0700

    Test commit hooks

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -10,11 +10,13 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
 
 bootstrap();
