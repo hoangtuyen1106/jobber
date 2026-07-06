daf86f9407a630bdb9ec777c9dc93692f82c33bc
tuyen <hoangtuyen1106@gmail.com>
2026-07-07 00:28:20 +0700

    18. Config Module

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
