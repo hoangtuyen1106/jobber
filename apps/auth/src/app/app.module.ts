8c389a812d28d9a5b66cdceeabf4d8670a709e04
tuyen <hoangtuyen1106@gmail.com>
2026-07-08 10:43:08 +0700

    21. JWT Module

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
       autoSchemaFile: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   providers: [],
 })
