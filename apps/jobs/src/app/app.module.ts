066fb7888481b6272abd6b10731c09be3fcd1e61
tuyen <hoangtuyen1106@gmail.com>
2026-07-22 16:11:19 +0700

    33. jobs playground settings

--- apps/jobs/src/app/app.module.ts
+++ apps/jobs/src/app/app.module.ts
@@ -11,6 +11,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
     }),
   ],
   controllers: [],
