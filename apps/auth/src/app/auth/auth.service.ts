5d32a503f7be60d1eabfb37cb90a37d60dcc4eeb
Tuyen <hoangtuyen1106@gmail.com>
2026-07-24 21:54:06 +0700

    36.gRPC Server

--- apps/auth/src/app/auth/auth.service.ts
+++ apps/auth/src/app/auth/auth.service.ts
@@ -41,7 +41,7 @@ export class AuthService {
         throw new UnauthorizedException();
       }
       return user;
-    } catch (err) {
+    } catch {
       throw new UnauthorizedException('Credentials are not valid');
     }
   }
