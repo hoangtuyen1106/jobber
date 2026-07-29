4cf13fbb6f63510ff7d1db3d294eeafb5afc6fb1
Tuyen <hoangtuyen1106@gmail.com>
2026-07-29 08:20:06 +0700

    .

--- apps/jobs/src/app/jobs/jobs.module.ts
+++ apps/jobs/src/app/jobs/jobs.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { FibonacciJob } from './fibonacci.job';
 import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 import { JobsService } from '../jobs.service';
 import { JobsResolver } from '../jobs.resolver';
@@ -7,6 +6,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 import { join } from 'path';
 import { PulsarModule } from '@jobber/pulsar';
+import { FibonacciJob } from './fibonacci/fibonacci.job';
 
 @Module({
   imports: [
