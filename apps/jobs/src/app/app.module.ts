4e6f5f8651e1600365dec495d51ef9159148e6ee
Tuyen <hoangtuyen1106@gmail.com>
2026-08-25 07:27:32 +0700

    95. Save Job

--- apps/jobs/src/app/app.module.ts
+++ apps/jobs/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { LoggerModule } from '@jobber/nestjs';
 import { GqlLoggingPlugin } from '@jobber/graphql';
 import { UploadsModule } from './uploads/uploads.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     LoggerModule,
     UploadsModule,
+    PrismaModule,
     ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     JobsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
--- apps/jobs/src/app/jobs/abstract.job.ts
+++ apps/jobs/src/app/jobs/abstract.job.ts
@@ -3,24 +3,37 @@ import { BadRequestException } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { Producer } from 'pulsar-client';
+import { PrismaService } from '../prisma/prisma.service';
+import { JobStatus } from '../models/job-status.enum';
 
 export abstract class AbstractJob<T extends object> {
   private producer: Producer;
   protected abstract messageClass: new () => T;
 
-  constructor(private readonly pulsarClient: PulsarClient) {}
+  constructor(
+    private readonly pulsarClient: PulsarClient,
+    private readonly prismaServer: PrismaService,
+  ) {}
 
   async execute(data: T, name: string) {
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(name);
     }
+    const job = await this.prismaServer.job.create({
+      data: {
+        name,
+        size: Array.isArray(data) ? data.length : 1,
+        completed: 0,
+        status: JobStatus.IN_PROGRESS,
+      },
+    });
     if (Array.isArray(data)) {
       for (const message of data) {
-        this.send(message);
+        this.send({ ...message, jobId: job.id });
       }
       return;
     }
-    this.send(data);
+    this.send({ ...data, jobId: job.id });
   }
 
   private send(data: T) {
