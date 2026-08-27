5f0e726eb51fd72e642d9cbcf44195d3f580f0ae
Tuyen <hoangtuyen1106@gmail.com>
2026-08-27 23:35:40 +0700

    DATABASE_URL

--- apps/auth/prisma.config.ts
+++ apps/auth/prisma.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url:
+      process.env.DATABASE_URL ||
+      'postgresql://user:password@localhost:5432/dummy',
   },
 });
